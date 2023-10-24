94ba794fa98b4a1c26ed9daea74116d4c469d65e
Cole Amantea <cole.ama@gmail.com>
2023-10-23 21:17:25 -0500

    Pretty much done

---
+++ EADbContext.cs
@@ -0,0 +1,34 @@
+﻿using EABackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Xml.Linq;
+
+namespace EABackEnd
+{
+    public class EADbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public EADbContext(DbContextOptions<EADbContext> context) : base(context) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(new User[]
+            {
+                new User { Id = 1, Email = "cole.ama@gmail.com", FirstName = "Cole", LastName = "Amantea", uid = ""}
+            });
+            modelBuilder.Entity<Event>().HasData(new Event[]
+            {
+                new Event {Id = 1, categoryId = 1, Description = "This Event is to clean up the Stones River", Title = "Clean Up Stones River", ScheduledDate = "10/25/2023", uid = ""}
+            });
+            modelBuilder.Entity<Category>().HasData(new Category[]
+            {
+                new Category { Id = 1, Name = "CleanUp"},
+                new Category { Id = 2, Name = "Volunteer"}
+            });
+        }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace EABackEnd.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string uid { get; set; }
+        public ICollection<User> Users { get; set; }
+        public string? ScheduledDate { get; set; }
+        public int categoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
